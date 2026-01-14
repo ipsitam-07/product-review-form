3dc1eba41671c71e3545fc2808f4f5ceab6781e4
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-01-14 15:51:32 +0530

    feat: add star highlight and select functionality

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,48 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', () => {
+    const starContainers = document.querySelectorAll('.star-rating');
+    //Helper functions
+    //Function to highlight stars
+    function highlightStars(container, value) {
+        const stars = container.querySelectorAll('.star');
+        stars.forEach((element) => {
+            const starValue = parseInt(element.getAttribute('data-value'));
+            if (starValue <= value) {
+                element.classList.add('active');
+            }
+            else {
+                element.classList.remove('active');
+            }
+        });
+    }
+    //Clear error function
+    function clearError(element) {
+        element.classList.remove('error');
+        const errorMsg = document.querySelector('.error-msg');
+        if (errorMsg)
+            errorMsg.innerHTML = '';
+    }
+    //star rating logic
+    starContainers.forEach((container) => {
+        const star = container.querySelectorAll('.star');
+        const hiddenInput = container.parentElement.querySelector('input[type="hidden"]');
+        star.forEach(star => {
+            star.addEventListener('mouseover', () => {
+                const value = parseInt(star.getAttribute('data-value'));
+                highlightStars(container, value);
+            });
+            star.addEventListener('click', () => {
+                const value = star.getAttribute('data-value');
+                hiddenInput.value = value;
+                container.setAttribute('data-selected', value);
+                highlightStars(container, value);
+                clearError(container.parentElement);
+            });
+        });
+        container.addEventListener('mouseleave', () => {
+            const selected = container.getAttribute('data-selected') || 0;
+            highlightStars(container, selected);
+        });
+    });
+});
+export {};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,52 @@
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', () => {
+    const starContainers : any = document.querySelectorAll('.star-rating');
+    //Helper functions
+    //Function to highlight stars
+    function highlightStars(container: any, value: any) : any {
+        const stars: any = container.querySelectorAll('.star');
+        stars.forEach((element: any) => {
+            const starValue: number = parseInt(element.getAttribute('data-value'));
+            if (starValue <= value){
+                element.classList.add('active');
+            } else {
+                element.classList.remove('active');
+            }
+        });
+    }
+
+    //Clear error function
+    function clearError(element: any) {
+        element.classList.remove('error');
+        const errorMsg = document.querySelector('.error-msg');
+        if(errorMsg) errorMsg.innerHTML = '';
+    }
+    //star rating logic
+
+    starContainers.forEach((container: any) => {
+        const star = container.querySelectorAll('.star');
+
+        const hiddenInput = container.parentElement.querySelector('input[type="hidden"]');
+
+        star.forEach(star => {
+            star.addEventListener('mouseover', () => {
+                const value: number = parseInt(star.getAttribute('data-value'));
+                highlightStars(container, value);
+            });
+
+            star.addEventListener('click', () =>{
+                const value = star.getAttribute('data-value');
+                hiddenInput.value = value;
+                container.setAttribute('data-selected', value);
+                highlightStars(container, value);
+
+                clearError(container.parentElement);
+            });
+        });
+
+        container.addEventListener('mouseleave', () => {
+            const selected = container.getAttribute('data-selected') || 0;
+            highlightStars(container,selected);
+        });
+    });
+
+})
