57a2ac6efcf7bdf5c8cca50a7a14b64178405941
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-01-16 11:38:14 +0530

    feat: add review and ratings types

---
+++ src/types/review.ts
@@ -0,0 +1,21 @@
+export type Ratings = {
+    overall: string;
+    quality: string;
+    value: string;
+    delivery?: string;
+    service?: string;
+}
+
+export type Review = {
+    id: string;
+    date: string;
+    title: string;
+    details: string;
+    rating: Ratings;
+    reviewType: 'Verified Purchase' | 'General Review';
+    tags?: string[];
+    recommend: string;
+    buyAgain?: boolean;
+    makePublic: boolean;
+    agreeTerms: boolean;
+}
