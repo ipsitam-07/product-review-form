94e323b8c0115097c208851dc4b953d33960068e
Ipsita Mohanty <ipsita.m@mindfiresolutions.com>
2026-01-20 15:48:51 +0530

    config commitlint

---
+++ src/types/review.ts
@@ -0,0 +1,52 @@
+export type Ratings = {
+  overall: string;
+  quality: string;
+  value: string;
+  delivery?: string;
+  service?: string;
+};
+
+export type ReviewType = 'Verified Purchase' | 'General Review';
+
+export type Review = {
+  id: string;
+  date: string;
+  title: string;
+  details: string;
+  rating: Ratings;
+  reviewType: ReviewType;
+  tags: string[];
+  recommend: string;
+  buyAgain?: boolean;
+  makePublic: boolean;
+  agreeTerms: boolean;
+};
+
+export type ReviewFormData = {
+  date: string;
+  title: string;
+  details: string;
+  rating: Ratings;
+  reviewType: ReviewType;
+  tags: string[];
+  recommend: string;
+  buyAgain: boolean;
+  makePublic: boolean;
+  agreeTerms: boolean;
+};
+
+export type ReviewFormUI = {
+  editId: string | null;
+  errors: Record<string, string>;
+};
+
+export type ReviewFormState = {
+  data: ReviewFormData;
+  ui: ReviewFormUI;
+};
+export type Theme = 'light' | 'dark';
+export type AppState = {
+  reviewForm: ReviewFormState;
+  reviews: Review[];
+  theme: Theme;
+};
